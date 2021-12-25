cb64e231c3dfe23214d3ccaa940fffa292172592
sbeknur <sbeknur2003@gmail.com>
2021-12-25 23:40:25 +0600

    Co-authored-by: ZalibiZ123 <ZalibiZ123@users.noreply.github.com>

---
+++ scripts/header.js
@@ -0,0 +1,17 @@
+// Когда пользователь прокручивает страницу, выполните myFunction
+window.onscroll = function() {myFunction()};
+
+// Получить навигатор
+var header = document.getElementById("header");
+
+// Получить смещение позиции навигационной панели
+var sticky = header.offsetTop;
+
+// Добавить класс sticky к навигационной панели, когда вы достигнете ее положения прокрутки. Удалите "sticky", когда вы покидаете положение прокрутки
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky")
+  } else {
+    header.classList.remove("sticky");
+  }
+}
