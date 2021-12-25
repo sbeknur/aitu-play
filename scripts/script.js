fd2723ee54588862014eace3dde9278cd52f9350
sbeknur <sbeknur2003@gmail.com>
2021-12-25 20:23:50 +0600

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function getSupportedPropertyName(properties) {
+    for (var i = 0; i < properties.length; i++) {
+        if (typeof document.body.style[properties[i]] != "undefined") {
+            return properties[i];
+        }
+    }
+    return null;
+}
+
+alert("Beknur LOX!!");
+  
+var transform = ["transform", "msTransform", "webkitTransform", "mozTransform", "oTransform"];
+  
+var item = document.querySelector(".intro__title_top");
+var transformProperty = getSupportedPropertyName(transform);
+  
+if (transformProperty) {
+    item.style[transformProperty] = translate3d(300, 0, 0);
+}
